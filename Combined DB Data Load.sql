de06abe913a1710edd1c7a23ca4452df4abf02d2
Jake Bilich <jabilich@gmail.com>
2016-08-02 21:50:00 -0600

    Combined DB Data Load.sql
    Queries used to populated the MSD subset MySQL database in the stmusic schema

---
+++ Combined DB Data Load.sql
@@ -0,0 +1,83 @@
+use stmusic;
+
+#==============================================================================================================================
+#			Patrice's Data Load
+#==============================================================================================================================
+
+-- create table artistLocation
+create table artistLocation (
+artistID varchar(50),
+latitude decimal(35,10),
+longitude decimal(35,10),
+artistName varchar(100),
+location varchar(100)); 
+
+-- load subset_artist_location.txt - artistLocation
+LOAD DATA LOCAL INFILE '/Users/pstockover/Desktop/dataproject/MillionSongSubset/AdditionalFiles/subset_artist_location.txt'
+INTO TABLE artistLocation FIELDS TERMINATED BY '<SEP>'; 
+
+-- create table tracksPerYear
+create table tracksPerYear(
+year int,
+trackID varchar(50),
+artistName varchar(100),
+songName varchar(250));
+
+-- load subset_tracks_per_year.txt - tracksPerYear
+LOAD DATA LOCAL INFILE '/Users/pstockover/Desktop/dataproject/MillionSongSubset/AdditionalFiles/subset_tracks_per_year.txt'
+INTO TABLE tracksPerYear FIELDS TERMINATED BY '<SEP>'; 
+
+-- create table firstSongArtist
+create table firstSongArtist (
+select artistName, songName, trackID, min(year) as Year
+from tracksPerYear
+group by artistName
+order by Year);
+
+
+#==============================================================================================================================
+#			Jake's Data Load
+#==============================================================================================================================
+
+
+#Create uniqueTracks Table
+create table if not exists  uniqueTracks (
+	trackID varchar(50),
+	songID varchar(50),
+    artistName varchar(350),
+    songName varchar(200)#,
+    #primary key (trackID)
+);
+#load the uniqueTracks tab delimited datafile 
+load data local infile 'C:\\Users/JacobBilich/Documents/MillionSongSubset/AdditionalFiles/subset_unique_tracks.txt' 	
+# formatting of the slashes in the path is important
+	into table uniqueTracks
+    character set latin1
+    fields terminated by '<SEP>' OPTIONALLY enclosed by '"' 	# tells the dB that there are fields with quotation marks
+    #lines terminated by '\r\n'
+    #ignore 1 lines
+    ;
+#show warnings;
+
+#select * from uniquetracks;
+
+
+#Create uniqueArtists Table
+create table if not exists  uniqueArtists (
+	artistID varchar(50),
+	artistMBID varchar(150),
+    trackID varchar(200),
+    artistName varchar(200)#,
+    #primary key (trackID)
+);
+#load the uniqueArtists tab delimited datafile 
+load data local infile 'C:\\Users/JacobBilich/Documents/MillionSongSubset/AdditionalFiles/subset_unique_artists.txt' 	
+# formatting of the slashes in the path is important
+	into table uniqueArtists
+    character set latin1
+    fields terminated by '<SEP>' OPTIONALLY enclosed by '"' 	# tells the dB that there are fields with quotation marks
+    #lines terminated by '\r\n'
+    #ignore 1 lines
+    ;
+#show warnings;
+
